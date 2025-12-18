cce723d4988181bc8c68e6659a5bd469f1231188
Alaa Abdulhussein <Alaa.Abdulhussein@numeruslocal.com>
2025-12-18 09:13:13 +0300

    chore: remove build and node_modules from repo

--- .nuxt/dist/server/_nuxt/DashboardNav-styles.NXDqfRLp.mjs
+++
@@ -1,4 +0,0 @@
-import style_0 from "./DashboardNav-styles-1.mjs-CD9f0XMo.js";
-export default [
-  style_0
-]
--- .nuxt/dist/server/_nuxt/error-500-styles.BDbon7bb.mjs
+++
@@ -1,4 +0,0 @@
-import style_0 from "./error-500-styles-1.mjs-9D18kP_v.js";
-export default [
-  style_0
-]
